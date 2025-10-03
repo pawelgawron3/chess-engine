34ad558439e7adbb32522d9e9879a74a6ad8ffbe
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-03 13:36:30 +0200

    Create Move.cs

---
+++ Chess/ChessEngine/Move.cs
@@ -0,0 +1,13 @@
+﻿namespace ChessEngine;
+
+public class Move
+{
+    public Position From { get; }
+    public Position To { get; }
+
+    public Move(Position from, Position to)
+    {
+        From = from;
+        To = to;
+    }
+}
