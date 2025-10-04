1f450ee93f83ba9ebb286ac83da30e96d39d4134
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-04 13:02:59 +0200

    Draw board

--- Chess/ChessUI/MainWindow.xaml.cs
+++ Chess/ChessUI/MainWindow.xaml.cs
@@ -1,22 +1,51 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ChessEngine;
 
 namespace ChessUI;
+
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
+    private Board board = new Board();
+
     public MainWindow()
     {
         InitializeComponent();
+        board.Initialize();
+        DrawBoard();
+    }
+
+    private void DrawBoard()
+    {
+        PiecesLayer.Children.Clear();
+
+        const int squareSize = 75;
+
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                Piece? piece = board.Squares[row, col];
+
+                if (piece != null)
+                {
+                    var img = new Image
+                    {
+                        Width = squareSize,
+                        Height = squareSize,
+                        Source = new BitmapImage(new Uri($"/Assets/{piece.Type}{(piece.Owner == Player.White ? "W" : "B")}.png", UriKind.Relative))
+                    };
+
+                    Canvas.SetLeft(img, col * squareSize);
+                    Canvas.SetTop(img, row * squareSize);
+
+                    PiecesLayer.Children.Add(img);
+                }
+            }
+        }
     }
 }
