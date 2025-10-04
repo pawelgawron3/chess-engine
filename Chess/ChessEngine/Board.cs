f11a5071d7fad97eceaf848252245312eb816f68
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-04 12:01:01 +0200

    Refactor the Initialize() method

--- Chess/ChessEngine/Board.cs
+++ Chess/ChessEngine/Board.cs
@@ -7,43 +7,24 @@ public class Board
 
     public void Initialize()
     {
-        // White pawns
+        // Pawns
         for (int col = 0; col < 8; col++)
         {
             Squares[6, col] = new Piece(PieceType.Pawn, Player.White);
-        }
-
-        // Black pawns
-        for (int col = 0; col < 8; col++)
-        {
             Squares[1, col] = new Piece(PieceType.Pawn, Player.Black);
         }
 
-        // Rooks
-        Squares[7, 0] = new Piece(PieceType.Rook, Player.White);
-        Squares[7, 7] = new Piece(PieceType.Rook, Player.White);
-        Squares[0, 0] = new Piece(PieceType.Rook, Player.Black);
-        Squares[0, 7] = new Piece(PieceType.Rook, Player.Black);
+        PieceType[] majorPieceOrder = new[]
+        {
+            PieceType.Rook, PieceType.Knight, PieceType.Bishop, PieceType.Queen,
+            PieceType.King, PieceType.Bishop, PieceType.Knight, PieceType.Rook
+        };
 
-        // Knights
-        Squares[7, 1] = new Piece(PieceType.Knight, Player.White);
-        Squares[7, 6] = new Piece(PieceType.Knight, Player.White);
-        Squares[0, 1] = new Piece(PieceType.Knight, Player.Black);
-        Squares[0, 6] = new Piece(PieceType.Knight, Player.Black);
-
-        // Bishops
-        Squares[7, 2] = new Piece(PieceType.Bishop, Player.White);
-        Squares[7, 5] = new Piece(PieceType.Bishop, Player.White);
-        Squares[0, 2] = new Piece(PieceType.Bishop, Player.Black);
-        Squares[0, 5] = new Piece(PieceType.Bishop, Player.Black);
-
-        // Queens
-        Squares[7, 3] = new Piece(PieceType.Queen, Player.White);
-        Squares[0, 3] = new Piece(PieceType.Queen, Player.Black);
-
-        // Kings
-        Squares[7, 4] = new Piece(PieceType.King, Player.White);
-        Squares[0, 4] = new Piece(PieceType.King, Player.Black);
+        for (int col = 0; col < 8; col++)
+        {
+            Squares[7, col] = new Piece(majorPieceOrder[col], Player.White);
+            Squares[0, col] = new Piece(majorPieceOrder[col], Player.Black);
+        }
     }
 
     public void MakeMove(Move move)
