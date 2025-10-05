1ca7bc2fbff0afb53430bee65d98417f0a853305
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-05 14:11:55 +0200

    Create GameState.cs

---
+++ Chess/ChessEngine/GameState.cs
@@ -0,0 +1,46 @@
+﻿namespace ChessEngine;
+
+public class GameState
+{
+    public Board Board { get; }
+    public Player CurrentPlayer => Board.CurrentPlayer;
+    public Position? SelectedPosition { get; private set; }
+    public List<Move> MoveHistory { get; } = new List<Move>();
+
+    public GameState(Board board)
+    {
+        Board = board;
+    }
+
+    public GameState()
+    {
+        Board = new Board();
+        Board.Initialize();
+    }
+
+    public IEnumerable<Move> GetPseudoLegalMoves()
+    {
+        return PseudoLegalMoveGenerator.GeneratePseudoLegalMoves(Board, CurrentPlayer);
+    }
+
+    public void SelectPosition(Position pos)
+    {
+        SelectedPosition = pos;
+    }
+
+    public void ClearSelection()
+    {
+        SelectedPosition = null;
+    }
+
+    public bool TryMakeMove(Move move)
+    {
+        if (Board.MakeMove(move))
+        {
+            MoveHistory.Add(move);
+            SelectedPosition = null;
+            return true;
+        }
+        return false;
+    }
+}
