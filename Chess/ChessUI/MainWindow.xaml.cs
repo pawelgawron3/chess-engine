69a9f881d9949047c65b02cd07ab9b6406b04c3d
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-05 21:29:00 +0200

    Create logic for handling moves

--- Chess/ChessUI/MainWindow.xaml.cs
+++ Chess/ChessUI/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using ChessEngine;
 
 namespace ChessUI;
@@ -48,8 +51,78 @@ public partial class MainWindow : Window
         }
     }
 
-    private void BoardGrid_MouseDown()
+    private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
     {
+        Point point = e.GetPosition(BoardGrid);
+        Position pos = GetBoardPositionFromClick(point);
 
+        HandleBoardClick(pos);
+    }
+
+    private Position GetBoardPositionFromClick(Point point)
+    {
+        const int squareSize = 75;
+        int col = (int)(point.X / squareSize);
+        int row = (int)(point.Y / squareSize);
+        return new Position(row, col);
+    }
+
+    private void HighlightMovesForSelectedPiece()
+    {
+        ClearHighlights();
+        if (_gameState.SelectedPosition == null) return;
+
+        var moves = _gameState.GetPseudoLegalMovesForPiece().ToList();
+
+        const int squareSize = 75;
+
+        foreach (var move in moves)
+        {
+            Ellipse ellipse = new Ellipse
+            {
+                Width = 40,
+                Height = 40,
+                Fill = Brushes.Yellow,
+                Opacity = 0.3,
+                IsHitTestVisible = false
+            };
+
+            Canvas.SetLeft(ellipse, move.To.Column * squareSize + (squareSize - 40) / 2);
+            Canvas.SetTop(ellipse, move.To.Row * squareSize + (squareSize - 40) / 2);
+
+            HighlightLayer.Children.Add(ellipse);
+        }
+    }
+
+    private void ClearHighlights()
+    {
+        HighlightLayer.Children.Clear();
+    }
+
+    private void HandleBoardClick(Position pos)
+    {
+        if (_gameState.SelectedPosition == null)
+        {
+            Piece? piece = _gameState.Board[pos];
+            if (piece != null && piece.Owner == _gameState.CurrentPlayer)
+            {
+                _gameState.SelectPosition(pos);
+                HighlightMovesForSelectedPiece();
+            }
+        }
+        else
+        {
+            Move move = new Move(_gameState.SelectedPosition.Value, pos);
+            if (_gameState.TryMakeMove(move))
+            {
+                ClearHighlights();
+                DrawBoard();
+            }
+            else
+            {
+                _gameState.ClearSelection();
+                ClearHighlights();
+            }
+        }
     }
 }
