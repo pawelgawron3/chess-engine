3b8798f3fded900461e1aca3513e04379d58518c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-07 17:47:14 +0200

    Create ChessCursors.cs

---
+++ Chess/ChessUI/ChessCursors.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ChessUI
+{
+    public static class ChessCursors
+    {
+        private static Cursor? _white;
+        private static Cursor? _black;
+
+        public static Cursor White => _white ??= LoadCursor("pack://application:,,,/ChessUI;component/Assets/CursorW.cur");
+
+        public static Cursor Black => _black ??= LoadCursor("pack://application:,,,/ChessUI;component/Assets/CursorB.cur");
+
+        private static Cursor LoadCursor(string uri)
+        {
+            var resourceInfo = Application.GetResourceStream(new Uri(uri, UriKind.Absolute))
+                ?? throw new FileNotFoundException($"Error: {uri}");
+
+            return new Cursor(resourceInfo.Stream, true);
+        }
+    }
+}
