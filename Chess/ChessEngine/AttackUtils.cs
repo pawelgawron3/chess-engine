251083ff85647a97e4cd82e40904f51fb6b77ffb
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-09 11:15:20 +0200

    Create AttackUtils.cs

---
+++ Chess/ChessEngine/AttackUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace ChessEngine;
+
+public static class AttackUtils
+{
+    public static Position? GetKingPosition(Board board, Player player)
+    {
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                Piece? piece = board[row, col];
+                if (piece != null && piece.Type == PieceType.King && piece.Owner == player)
+                {
+                    return new Position(row, col);
+                }
+            }
+        }
+
+        return null;
+    }
+}
