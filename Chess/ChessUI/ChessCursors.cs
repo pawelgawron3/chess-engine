5800f69c7cac20e2f4d334d1bf3d2d229fd8eb02
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-16 13:43:36 +0200

    Cleanup doc using CodeMaid extension

--- Chess/ChessUI/ChessCursors.cs
+++ Chess/ChessUI/ChessCursors.cs
@@ -2,23 +2,22 @@
 using System.Windows;
 using System.Windows.Input;
 
-namespace ChessUI
+namespace ChessUI;
+
+public static class ChessCursors
 {
-    public static class ChessCursors
+    private static Cursor? _white;
+    private static Cursor? _black;
+
+    public static Cursor White => _white ??= LoadCursor("pack://application:,,,/ChessUI;component/Assets/Images/CursorW.cur");
+
+    public static Cursor Black => _black ??= LoadCursor("pack://application:,,,/ChessUI;component/Assets/Images/CursorB.cur");
+
+    private static Cursor LoadCursor(string uri)
     {
-        private static Cursor? _white;
-        private static Cursor? _black;
+        var resourceInfo = Application.GetResourceStream(new Uri(uri, UriKind.Absolute))
+            ?? throw new FileNotFoundException($"Error: {uri}");
 
-        public static Cursor White => _white ??= LoadCursor("pack://application:,,,/ChessUI;component/Assets/Images/CursorW.cur");
-
-        public static Cursor Black => _black ??= LoadCursor("pack://application:,,,/ChessUI;component/Assets/Images/CursorB.cur");
-
-        private static Cursor LoadCursor(string uri)
-        {
-            var resourceInfo = Application.GetResourceStream(new Uri(uri, UriKind.Absolute))
-                ?? throw new FileNotFoundException($"Error: {uri}");
-
-            return new Cursor(resourceInfo.Stream, true);
-        }
+        return new Cursor(resourceInfo.Stream, true);
     }
 }
