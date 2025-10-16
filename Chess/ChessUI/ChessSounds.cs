d73bd8a49baebb0fbf5a7a73958280a7a7fa0e53
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-16 13:16:04 +0200

    Create ChessSounds.cs

---
+++ Chess/ChessUI/ChessSounds.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System.Media;
+using ChessEngine;
+
+namespace ChessUI;
+
+public static class ChessSounds
+{
+    private static readonly string BasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets/Sounds");
+
+    private static readonly SoundPlayer MoveSound = new SoundPlayer(Path.Combine(BasePath, "move-self.wav"));
+    private static readonly SoundPlayer CaptureSound = new SoundPlayer(Path.Combine(BasePath, "capture.wav"));
+    private static readonly SoundPlayer PromoteSound = new SoundPlayer(Path.Combine(BasePath, "promote.wav"));
+    private static readonly SoundPlayer CastleSound = new SoundPlayer(Path.Combine(BasePath, "castle.wav"));
+
+    static ChessSounds()
+    {
+        try
+        {
+            MoveSound.Load();
+            CaptureSound.Load();
+            PromoteSound.Load();
+            CastleSound.Load();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Sound load error: {ex.Message}");
+        }
+    }
+
+    public static void PlaySoundForMove(Move move, Piece? capturedPiece)
+    {
+        switch (move.Type)
+        {
+            case MoveType.Normal:
+                if (capturedPiece == null)
+                    PlayMoveSound();
+                else
+                    PlayCaptureSound();
+                break;
+
+            case MoveType.Castling:
+                PlayCastleSound();
+                break;
+
+            case MoveType.Promotion:
+                PlayPromoteSound();
+                break;
+
+            case MoveType.EnPassant:
+                PlayCaptureSound();
+                break;
+        }
+    }
+
+    private static void PlayMoveSound()
+    {
+        MoveSound.Play();
+    }
+
+    private static void PlayCaptureSound()
+    {
+        CaptureSound.Play();
+    }
+
+    private static void PlayPromoteSound()
+    {
+        PromoteSound.Play();
+    }
+
+    private static void PlayCastleSound()
+    {
+        CastleSound.Play();
+    }
+}
