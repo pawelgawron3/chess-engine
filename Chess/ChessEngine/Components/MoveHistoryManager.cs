1d8ab46278d9beff7e9a8c38d4fdf07f83ceedae
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-10-29 14:13:13 +0100

    Create MoveHistoryManager.cs

---
+++ Chess/ChessEngine/Components/MoveHistoryManager.cs
@@ -0,0 +1,19 @@
+﻿namespace ChessEngine.Components;
+
+public class MoveHistoryManager
+{
+    public List<MoveRecord> MoveHistory { get; } = new();
+
+    public void Add(MoveRecord record) => MoveHistory.Add(record);
+
+    public MoveRecord? Undo(Board board)
+    {
+        if (MoveHistory.Count == 0)
+            return null;
+
+        MoveRecord? last = MoveHistory[^1];
+        MoveHistory.RemoveAt(MoveHistory.Count - 1);
+        board.UndoMove(last);
+        return last;
+    }
+}
