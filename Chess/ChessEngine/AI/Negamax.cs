faf8d198795f59b78a133a3595b079fadb75cd10
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-06 12:50:03 +0100

    Add ctor to Negamax class

--- Chess/ChessEngine/AI/Negamax.cs
+++ Chess/ChessEngine/AI/Negamax.cs
@@ -1,5 +1,11 @@
 ﻿namespace ChessEngine.AI;
+
 public class Negamax
 {
+    private readonly IEvaluationFunction _evaluator;
 
+    public Negamax(IEvaluationFunction evaluator)
+    {
+        _evaluator = evaluator;
+    }
 }
