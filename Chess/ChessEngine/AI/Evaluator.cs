2213f3ad01fc2e15b669a41f5d9d720d62fdb19b
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-06 14:58:06 +0100

    Add _pieceValues private field

--- Chess/ChessEngine/AI/Evaluator.cs
+++ Chess/ChessEngine/AI/Evaluator.cs
@@ -2,6 +2,16 @@
 
 public class Evaluator : IEvaluationFunction
 {
+    private static readonly Dictionary<PieceType, int> _pieceValues = new()
+    {
+        { PieceType.Pawn, 100},
+        { PieceType.Knight, 300},
+        { PieceType.Bishop, 310},
+        { PieceType.Rook, 500},
+        { PieceType.Queen, 900},
+        { PieceType.King, 10_000}
+    };
+
     public int Evaluate(GameState state)
     {
         throw new NotImplementedException();
