a9c71a2a98754501dfbf62e88bd1057248cd5bda
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-07 15:29:18 +0100

    Implement Negamax algorithm

--- Chess/ChessEngine/AI/Negamax.cs
+++ Chess/ChessEngine/AI/Negamax.cs
@@ -8,4 +8,45 @@ public class Negamax
     {
         _evaluator = evaluator;
     }
+
+    public (Move? BestMove, int Score) Search(GameState state, int depth)
+    {
+        int bestScore = int.MinValue;
+        Move? bestMove = null;
+
+        foreach (var move in LegalMoveGenerator.GenerateLegalMoves(state))
+        {
+            state.TryMakeMove(move);
+            int score = -NegamaxSearch(state, depth - 1);
+            state.TryUndoMove();
+
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+
+        return (bestMove, bestScore);
+    }
+
+    private int NegamaxSearch(GameState state, int depth)
+    {
+        if (depth == 0 || state.GameResult != null)
+            return _evaluator.Evaluate(state) * (int)state.CurrentPlayer;
+
+        int bestScore = int.MinValue;
+
+        foreach (var move in LegalMoveGenerator.GenerateLegalMoves(state))
+        {
+            state.TryMakeMove(move);
+            int score = -NegamaxSearch(state, depth - 1);
+            state.TryUndoMove();
+
+            if (score > bestScore)
+                bestScore = score;
+        }
+
+        return bestScore;
+    }
 }
