e723ae2c7f0285e388befe11bb85387ab1cbd77b
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-08 11:46:59 +0100

    Update the values of chess pieces
    The _pieceValues dictionary has been updated to use the chess piece values proposed by Tomasz Michniewski in his article.

--- Chess/ChessEngine/AI/Evaluator.cs
+++ Chess/ChessEngine/AI/Evaluator.cs
@@ -5,11 +5,11 @@ public class Evaluator : IEvaluationFunction
     private static readonly Dictionary<PieceType, int> _pieceValues = new()
     {
         { PieceType.Pawn, 100},
-        { PieceType.Knight, 300},
-        { PieceType.Bishop, 310},
+        { PieceType.Knight, 320},
+        { PieceType.Bishop, 330},
         { PieceType.Rook, 500},
         { PieceType.Queen, 900},
-        { PieceType.King, 10_000}
+        { PieceType.King, 20_000}
     };
 
     public int Evaluate(GameState state)
