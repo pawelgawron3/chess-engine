da4e7107a1c741df916f087091f572fb65b1adb4
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-12 12:36:34 +0100

    Create CastlingRights.cs

---
+++ Chess/ChessEngine/CastlingRights.cs
@@ -0,0 +1,14 @@
+﻿namespace ChessEngine;
+
+public struct CastlingRights
+{
+    public (bool KingMoved, bool RookAMoved, bool RookHMoved) White;
+    public (bool KingMoved, bool RookAMoved, bool RookHMoved) Black;
+
+    public CastlingRights((bool, bool, bool) white, (bool, bool, bool) black)
+    {
+        White = white; Black = black;
+    }
+
+    public CastlingRights Clone() => new(White, Black);
+}
