6791d0ec093e85480ffc71f99febc6085df1c4ca
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-15 18:10:58 +0100

    Create TranspositionTable.cs

---
+++ Chess/ChessEngine/AI/TranspositionTable.cs
@@ -0,0 +1,7 @@
+﻿namespace ChessEngine.AI;
+
+public static class TranspositionTable
+{
+    public const int Size = 1 << 22; // 4_194_304
+    public static readonly TTEntry[] Table = new TTEntry[Size];
+}
