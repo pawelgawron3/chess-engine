b0d8553288ad730dfe2490ef0bc802f6b628c352
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-15 18:10:43 +0100

    Create TTEntry.cs

---
+++ Chess/ChessEngine/AI/TTEntry.cs
@@ -0,0 +1,28 @@
+﻿using ChessEngine.Chessboard;
+
+namespace ChessEngine.AI;
+
+public enum BoundType
+{
+    Exact,
+    LowerBound,
+    UpperBound
+}
+
+public struct TTEntry
+{
+    public ulong Hash;
+    public int Depth;
+    public int Score;
+    public BoundType Bound;
+    // public Move? BestMove;
+
+    public TTEntry(ulong hash, int depth, int score, BoundType bound) // Move? bestMove
+    {
+        Hash = hash;
+        Depth = depth;
+        Score = score;
+        Bound = bound;
+        // BestMove = bestMove;
+    }
+}
