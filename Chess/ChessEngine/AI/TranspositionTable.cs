79eab54e95072ce2afe845f8f1f63bbbe8ec81e5
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-16 17:04:37 +0100

    Add Probe() and Store() methods

--- Chess/ChessEngine/AI/TranspositionTable.cs
+++ Chess/ChessEngine/AI/TranspositionTable.cs
@@ -1,7 +1,22 @@
-﻿namespace ChessEngine.AI;
+﻿using ChessEngine.Chessboard;
+
+namespace ChessEngine.AI;
 
 public static class TranspositionTable
 {
     public const int Size = 1 << 22; // 4_194_304
     public static readonly TTEntry[] Table = new TTEntry[Size];
+
+    public static ref TTEntry Probe(ulong hash)
+    {
+        return ref Table[hash & (Size - 1)];
+    }
+
+    public static void Store(ulong hash, int depth, int score, BoundType bound, Move? best)
+    {
+        ref TTEntry entry = ref Probe(hash);
+
+        if (entry.Hash != hash || depth >= entry.Depth)
+            entry = new TTEntry(hash, depth, score, bound, best);
+    }
 }
