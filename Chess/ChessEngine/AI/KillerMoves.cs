c72f84de11ece89b40c0994a06139229664361ca
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-21 17:20:28 +0100

    Create KillerMoves.cs

---
+++ Chess/ChessEngine/AI/KillerMoves.cs
@@ -0,0 +1,29 @@
+﻿using ChessEngine.Chessboard;
+
+namespace ChessEngine.AI;
+
+public static class KillerMoves
+{
+    public const int MaxKillerMoves = 2;
+
+    public static readonly Move?[] Killer1 = new Move?[8];
+    public static readonly Move?[] Killer2 = new Move?[8];
+
+    public static void AddKillerMove(int depth, Move move)
+    {
+        if (Killer1[depth] == null)
+        {
+            Killer1[depth] = move;
+            return;
+        }
+
+        if (Killer2[depth] == null)
+        {
+            Killer2[depth] = move;
+            return;
+        }
+
+        Killer2[depth] = Killer1[depth];
+        Killer1[depth] = move;
+    }
+}
