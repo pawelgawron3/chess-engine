3f31b888de0413a7a0e8027b1be37138527fae31
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-21 14:30:50 +0100

    Create MovePicker.cs
    Created MovePicker.cs for Move Ordering. Implemented a MVV-LVA strategy.

---
+++ Chess/ChessEngine/AI/MovePicker.cs
@@ -0,0 +1,75 @@
+﻿using ChessEngine.Chessboard;
+using ChessEngine.Game;
+using ChessEngine.Utils;
+
+namespace ChessEngine.AI;
+
+public class MovePicker
+{
+    private readonly Move[] _moves;
+    private readonly int[] _scores;
+    private int _index = 0;
+
+    public MovePicker(IEnumerable<Move> moves, GameState state)
+    {
+        _moves = moves.ToArray();
+        _scores = new int[_moves.Length];
+
+        ScoreMoves(state);
+        SortMoves();
+    }
+
+    private void ScoreMoves(GameState state)
+    {
+        for (int i = 0; i < _moves.Length; i++)
+        {
+            var move = _moves[i];
+            Piece? capturedPiece = AttackUtils.GetCapturedPiece(state.Board, move);
+
+            if (capturedPiece != null)
+            {
+                int victim = PieceValues.Value[(int)capturedPiece.Value.Type];
+                int attacker = PieceValues.Value[(int)state.Board[move.From.Row, move.From.Column]!.Value.Type];
+
+                // MVV-LVA
+                _scores[i] = victim * 10 - attacker;
+            }
+            else
+            {
+                _scores[i] = 0;
+            }
+        }
+    }
+
+    private void SortMoves()
+    {
+        for (int i = 1; i < _moves.Length; i++)
+        {
+            int score = _scores[i];
+            Move move = _moves[i];
+            int j = i - 1;
+
+            while (j >= 0 && _scores[j] < score)
+            {
+                _scores[j + 1] = _scores[j];
+                _moves[j + 1] = _moves[j];
+                j--;
+            }
+
+            _scores[j + 1] = score;
+            _moves[j + 1] = move;
+        }
+    }
+
+    public bool TryGetNext(out Move move)
+    {
+        if (_index >= _moves.Length)
+        {
+            move = default;
+            return false;
+        }
+
+        move = _moves[_index++];
+        return true;
+    }
+}
