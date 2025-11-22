099c0e873a344fc516aac7ca6be5ffd333a4d670
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-22 11:48:10 +0100

    Refactor KillerMoves.cs

--- Chess/ChessEngine/AI/KillerMoves.cs
+++ Chess/ChessEngine/AI/KillerMoves.cs
@@ -4,15 +4,33 @@ namespace ChessEngine.AI;
 
 public static class KillerMoves
 {
-    public const int MaxKillerMoves = 2;
-    public static readonly Move?[,] KillerMovesTable = new Move?[6, MaxKillerMoves];
+    private static Move?[,] _table = new Move?[0, 0];
+    private const int _maxKillerMoves = 2;
+    private static int _maxDepth;
+
+    public static void Init(int depth)
+    {
+        _maxDepth = depth;
+        _table = new Move?[depth + 1, _maxKillerMoves];
+    }
 
     public static void AddKillerMove(int depth, Move move)
     {
-        if (KillerMovesTable[depth, 0] != move)
+        if (depth < 0 || depth > _maxDepth)
+            return;
+
+        if (_table[depth, 0] != move)
         {
-            KillerMovesTable[depth, 1] = KillerMovesTable[depth, 0];
-            KillerMovesTable[depth, 0] = move;
+            _table[depth, 1] = _table[depth, 0];
+            _table[depth, 0] = move;
         }
+    }
+
+    public static Move? Get(int depth, int index)
+    {
+        if (depth < 0 || depth > _maxDepth)
+            return null;
+
+        return _table[depth, index];
     }
 }
