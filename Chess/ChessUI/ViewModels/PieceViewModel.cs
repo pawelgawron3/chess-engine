7a9f2a6f5129ce8779360c62bb906f4176e29d03
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-27 12:40:08 +0100

    Create PieceViewModel.cs

---
+++ Chess/ChessUI/ViewModels/PieceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace ChessUI.ViewModels;
+
+public class PieceViewModel
+{
+    public required ImageSource Image { get; set; }
+    public double Left { get; set; }
+    public double Top { get; set; }
+    public double Size { get; set; }
+}
