18db241bebb9eab04b63f0eb11d7c288c7276d8e
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-27 21:08:26 +0100

    Create HistoryHeuristicTable.cs

---
+++ Chess/ChessEngine/AI/HistoryHeuristicTable.cs
@@ -0,0 +1,23 @@
+﻿using ChessEngine.Chessboard;
+
+namespace ChessEngine.AI;
+
+public static class HistoryHeuristicTable
+{
+    private static int[,] _table = new int[64, 64];
+
+    public static void Add(Move move, int depth)
+    {
+        _table[move.From.ToIndex(), move.To.ToIndex()] += depth * depth;
+    }
+
+    public static int Get(Move move)
+    {
+        return _table[move.From.ToIndex(), move.To.ToIndex()];
+    }
+
+    public static void Reset()
+    {
+        Array.Clear(_table, 0, _table.Length);
+    }
+}
