e35bdd1d8d2473489d9b48c3140945d7d3229ace
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-27 12:54:14 +0100

    Refactor the MainWindow code-behind file

---
+++ Chess/ChessUI/Views/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using ChessEngine;
+using ChessEngine.Game;
+using ChessUI.Services;
+using ChessUI.ViewModels;
+
+namespace ChessUI.Views;
+
+public partial class MainWindow : Window
+{
+    private readonly MainViewModel _vm;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _vm = (MainViewModel)DataContext;
+
+        Cursor = ChessCursors.White;
+        _vm.GameState.PropertyChanged += OnGameStatePropertyChanged;
+    }
+
+    private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        Point pt = e.GetPosition(BoardGrid);
+        _vm.OnBoardClick(pt);
+    }
+
+    private void OnGameStatePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(GameState.CurrentPlayer))
+        {
+            Player player = _vm.GameState.CurrentPlayer;
+
+            Cursor = (player == Player.White) ? ChessCursors.White : ChessCursors.Black;
+        }
+    }
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        base.OnKeyDown(e);
+        if (e.Key == Key.Left)
+        {
+            _vm.UndoCommand.Execute(null);
+        }
+    }
+}
