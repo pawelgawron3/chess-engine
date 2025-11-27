d2def75d83090ed4b29920e292803b20031f59f5
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-27 12:40:30 +0100

    Create HighlightViewModel.cs

---
+++ Chess/ChessUI/ViewModels/HighlightViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media;
+
+namespace ChessUI.ViewModels;
+
+public class HighlightViewModel
+{
+    public double Left { get; set; }
+
+    public double Top { get; set; }
+    public double Size { get; set; }
+    public Brush? FillBrush { get; set; }
+
+    public Brush? StrokeBrush { get; set; }
+    public double StrokeThickness { get; set; }
+}
