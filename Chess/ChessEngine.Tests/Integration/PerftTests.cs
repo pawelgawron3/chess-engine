3f3c04ed66cc4ce2db3e7a7c7411c83ca739a8c0
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-28 17:39:57 +0100

    Create PerftTests.cs

---
+++ Chess/ChessEngine.Tests/Integration/PerftTests.cs
@@ -0,0 +1,19 @@
+﻿using ChessEngine.Game;
+
+namespace ChessEngine.Tests.Integration;
+
+public class PerftTests
+{
+    [Theory]
+    [InlineData(1, 20UL)]
+    [InlineData(2, 400UL)]
+    [InlineData(3, 8902UL)]
+    [InlineData(4, 197281UL)]
+    [InlineData(5, 4865609UL)]
+    public void Perft_StartPosition(int depth, ulong expected)
+    {
+        var state = new GameStateEngine();
+        ulong nodes = Perft.Run(state, depth);
+        Assert.Equal(expected, nodes);
+    }
+}
