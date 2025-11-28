537efe014e9226396bf92eb470dd7eac242e4c45
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-28 12:54:07 +0100

    Create MoveStruct.cs

---
+++ Chess/ChessEngine/Chessboard/MoveStruct.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessEngine.Chessboard;
+
+public struct MoveStruct
+{
+    public CastlingRights CastlingRightsBefore;
+    public int? EnPassantFileBefore;
+    public int HalfMoveClockBefore;
+    public ulong HashBefore;
+    public Piece? CapturedPiece;
+}
