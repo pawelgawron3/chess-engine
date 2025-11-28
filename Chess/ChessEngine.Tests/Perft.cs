15e3f60968cb0121ae7ee54847dc89453a1623d9
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-28 17:39:31 +0100

    Create Perft.cs

---
+++ Chess/ChessEngine.Tests/Perft.cs
@@ -0,0 +1,24 @@
+﻿using ChessEngine.Game;
+using ChessEngine.MoveGeneration;
+
+namespace ChessEngine.Tests;
+
+public static class Perft
+{
+    public static ulong Run(GameStateEngine state, int depth)
+    {
+        if (depth == 0)
+            return 1;
+
+        ulong nodes = 0;
+
+        foreach (var move in LegalMoveGenerator.GenerateLegalMoves(state))
+        {
+            state.Services.EngineMakeMove(move, out var undo);
+            nodes += Run(state, depth - 1);
+            state.Services.EngineUndoMove(move, undo);
+        }
+
+        return nodes;
+    }
+}
