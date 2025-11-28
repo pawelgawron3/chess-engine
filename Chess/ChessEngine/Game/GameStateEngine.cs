70efa81a096e2af1b823d8e68b7e66573667d158
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-28 17:02:04 +0100

    Create GameStateEngine.cs

---
+++ Chess/ChessEngine/Game/GameStateEngine.cs
@@ -0,0 +1,25 @@
+﻿using ChessEngine.Chessboard;
+using ChessEngine.Components;
+using ChessEngine.MoveGeneration;
+
+namespace ChessEngine.Game;
+
+public class GameStateEngine
+{
+    public Board Board { get; }
+    public Player CurrentPlayer { get; set; } = Player.White;
+
+    public GameResult? GameResult { get; set; }
+    public GameServices Services { get; }
+
+    public GameStateEngine()
+    {
+        Board = new Board();
+        Board.Initialize();
+        Services = new GameServices(this);
+    }
+
+    public IEnumerable<Move> GetLegalMoves() => LegalMoveGenerator.GenerateLegalMoves(this);
+
+    public IEnumerable<Move> GetLegalMovesForPiece(Position pos) => LegalMoveGenerator.GenerateLegalMovesForPiece(this, pos);
+}
