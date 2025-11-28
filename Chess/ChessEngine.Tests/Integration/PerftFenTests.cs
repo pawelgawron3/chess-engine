11ed4f6f30c6751a9c6ce2e6f1f0017c383d56e9
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-28 18:08:09 +0100

    Create PerftFenTests.cs

---
+++ Chess/ChessEngine.Tests/Integration/PerftFenTests.cs
@@ -0,0 +1,21 @@
+﻿using ChessEngine.Game;
+using ChessEngine.Utils;
+
+namespace ChessEngine.Tests.Integration;
+
+public class PerftFenTests
+{
+    [Theory]
+    [InlineData("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -", 1, 20UL)]
+    [InlineData("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -", 2, 400UL)]
+    [InlineData("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 1, 48UL)]
+    [InlineData("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 2, 2039UL)]
+    public void Perft_FromFen(string fen, int depth, ulong expected)
+    {
+        GameStateEngine state = new();
+        FenLoader.LoadFen(state, fen);
+
+        ulong nodes = Perft.Run(state, depth);
+        Assert.Equal(expected, nodes);
+    }
+}
