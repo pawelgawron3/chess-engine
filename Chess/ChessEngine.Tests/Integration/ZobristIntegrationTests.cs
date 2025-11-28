a2152eca0ab1699c6f31bf593f8451d9ea3ddd8b
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-28 17:06:21 +0100

    Delete the MakeUndo_MustRestoreZobristHash() test

--- Chess/ChessEngine.Tests/Integration/ZobristIntegrationTests.cs
+++ Chess/ChessEngine.Tests/Integration/ZobristIntegrationTests.cs
@@ -6,24 +6,9 @@ namespace ChessEngine.Tests.Integration;
 public class ZobristIntegrationTests
 {
     [Fact]
-    public void MakeUndo_MustRestoreZobristHash()
-    {
-        GameState state = new GameState();
-        var initialHash = state.Services.Hasher.CurrentHash;
-
-        foreach (var move in LegalMoveGenerator.GenerateLegalMoves(state))
-        {
-            state.TryMakeMove(move);
-            state.TryUndoMove();
-
-            Assert.Equal(initialHash, state.Services.Hasher.CurrentHash);
-        }
-    }
-
-    [Fact]
     public void MakeUndoEngine_MustRestoreZobristHash()
     {
-        GameState state = new GameState();
+        GameStateEngine state = new GameStateEngine();
         var initialHash = state.Services.Hasher.CurrentHash;
 
         foreach (var move in LegalMoveGenerator.GenerateLegalMoves(state))
