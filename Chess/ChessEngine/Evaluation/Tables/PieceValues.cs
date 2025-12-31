180b748c42d8ef92c65ee0105cfd6c0b5b6f6bea
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-12-31 18:13:20 +0100

    Add a comment for better clarity

--- Chess/ChessEngine/Evaluation/Tables/PieceValues.cs
+++ Chess/ChessEngine/Evaluation/Tables/PieceValues.cs
@@ -4,6 +4,7 @@ public static class PieceValues
 {
     public static readonly int[] Value = new int[6]
     {
+        // pawn, knight, bishop, rook, queen, king
         100, 320, 330, 500, 900, 20_000
     };
 }
